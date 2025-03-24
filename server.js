513b2c45aa3c3d8b604e92fd0a70e62907e9b9ab
Mayank Choudhary <baggarmrjat@gmail.com>
2025-03-25 02:03:29 +0530

    Added start script43

--- server.js
+++ server.js
@@ -73,5 +73,5 @@ io.on("connection", (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 server.listen(PORT, () => console.log(Server Running on Port ${PORT}));
